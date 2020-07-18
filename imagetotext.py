d09eb313173349ac3f0d793eb943b585b7fa2359
Shubham Gupta <mysg147@gmail.com>
2020-07-18 18:03:44 +0530

    json file created

--- imagetotext.py
+++ imagetotext.py
@@ -1,13 +1,19 @@
 import pytesseract
 import os
 from PIL import Image
+import json
 
 
 pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract" #Path to the tesseract 
 
 def convert():
-    img = Image.open('img.png')
+    data={}
+    image_name=input()
+    img = Image.open(image_name)
     text = pytesseract.image_to_string(img)
+    data[image_name]={"text":text}
+    with open('imageToText.json','w') as fp:
+        json.dump(data,fp,indent=4)
     print(text)
 
 convert()
